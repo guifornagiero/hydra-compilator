837b6e9697ed003e355b3c211927de7c2acda1af
guifornagiero <guifornagiero.c@hotmail.com>
2024-09-24 19:51:05 -0300

    feat: adicionar analisador lexico

---
+++ src/Analysers/Lexer.ts
@@ -0,0 +1,56 @@
+import { AFD } from "../AFD/AFD";
+import Identifier from "../AFD/Identifier";
+import LiteralNumber from "../AFD/LiteralNumber";
+import SingleToken from "../AFD/SingleToken";
+import { Token } from "../Models/Token";
+
+export default class Lexer {
+    private tokens: Token[];
+    private afds: AFD[];
+    private code: string[];
+
+    constructor(code: string) {
+        this.tokens = [];
+        this.afds = [];
+        this.code = code.split("");
+
+        this.afds.push(new SingleToken(), new LiteralNumber(), new Identifier());
+    }
+
+    private skipWhiteSpace(): void {
+        while (this.code[0] === " " || this.code[0] === "\n") {
+            this.code.shift();
+        }
+    }
+
+    public getTokens(): Token[] {
+        let accepted: boolean;
+
+        while (this.code.length > 0) {
+            accepted = false;
+            this.skipWhiteSpace();
+
+            if (this.code.length == 0) break;
+
+            for (let afd of this.afds) {
+                const reservCode = [...this.code];
+                const token = afd.evaluate(this.code);
+
+                if (!!token) {
+                    accepted = true;
+                    this.tokens.push(token);
+                    break;
+                } else {
+                    this.code = reservCode;
+                }
+            }
+
+            if (accepted) continue;
+            
+            throw new Error("Token not recognized: " + this.code[0])
+        }
+
+        this.tokens.push(new Token("EOF", "$"));
+        return this.tokens;
+    }
+}
